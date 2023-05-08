38051bece3a6d2b7b264f106e26493d3c11a29ca
raor13 <rama.rao@roche.com>
2023-05-08 22:41:50 +0800

    added lifetime_value column

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -6,14 +6,20 @@ orders as (
     select * from {{ ref('stg_orders') }}
 ),
 
+order_amount as (
+    select * from {{ ref('fct_orders') }}
+),
+
 customer_orders as (
     select
         customer_id,
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        min(a.order_date) as first_order_date,
+        max(a.order_date) as most_recent_order_date,
+        count(a.order_id) as number_of_orders,
+        sum(b.amount) as lifetime_value
     from
-        orders
+        orders a
+    join order_amount b using (customer_id, order_id)
     group by 1
 ),
 
@@ -24,7 +30,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(customer_orders.lifetime_value, 0) as lifetime_value
     from
         customers
     left join customer_orders using (customer_id)
